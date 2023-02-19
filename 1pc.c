ddfa3ae3c6c0047d3034991bc15613a6f0fd2b27
Mahiro Fukuda <mahiro.fukuda.aries@gmail.com>
2023-02-19 08:12:10 +0000

    add tokniser

--- 1pc.c
+++ 1pc.c
@@ -1,5 +1,115 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
+// 真を返す。それ以外の場合には偽を返す。
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("'%c'ではありません", op);
+  token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error("数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token)); // メモリの確保
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p) {
+  // ダミーのヘッドに繋げていく
+  // こうすることで実装が楽になる
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // 記号トークン
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    // 文字が数字の場合にはvalにその数字をセットする
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("トークナイズできません");
+  }
+
+  // 全部終わったら終了文字としてTokenに記録
+  new_token(TK_EOF, cur, p);
+  return head.next; // これはつまりcurを返してる?
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,34 +117,32 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  // トークナイズする
+  token = tokenize(argv[1]);
 
+  // アセンブリの前半部分を出力
   printf(".globl main\n");
   printf("main:\n");
-  // strolで読み込んだ後次のポインタを返す
-  // これで順々に見れる
-  printf("  mov x0, %ld\n", strtol(p, &p, 10));
 
-  // 加減コンパイラ
-  // 読み込みながら次の文字を返していくのを繰り返して式全体を読み込む
-  while (*p) {
-    if (*p == '+') {
-      p++; // 1文字進める
-      printf("  add x0, x0, %ld\n", strtol(p, &p, 10));
+  // 式の最初は数でなければならないので、それをチェックして
+  // 最初のmov命令を出力
+  printf("  mov x0, %d\n", expect_number());
+
+  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
+  // アセンブリを出力
+  while (!at_eof()) {
+    // [+]かどうかチェックして、数字を読み取って加算
+    if (consume('+')) {
+      printf(" add x0, x0, %d\n", expect_number());
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub x0, x0, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    return 1;
+    // [-]かどうかチェックして数字を読み取って減算数
+    expect('-');
+    printf(" sub x0, x0, %d\n", expect_number());
   }
 
-
-  printf("  ret\n");
+  // returnのアセンブリを出力
+  printf(" ret\n");
   return 0;
 }
