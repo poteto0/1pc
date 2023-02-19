c4036794e7a1f37355d3284861b713c8fdcaf5de
Mahiro Fukuda <mahiro.fukuda.aries@gmail.com>
2023-02-19 07:44:32 +0000

    add and sub

--- 1pc.c
+++ 1pc.c
@@ -7,9 +7,34 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov x0, %d\n", atoi(argv[1]));
+  // strolで読み込んだ後次のポインタを返す
+  // これで順々に見れる
+  printf("  mov x0, %ld\n", strtol(p, &p, 10));
+
+  // 加減コンパイラ
+  // 読み込みながら次の文字を返していくのを繰り返して式全体を読み込む
+  while (*p) {
+    if (*p == '+') {
+      p++; // 1文字進める
+      printf("  add x0, x0, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub x0, x0, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
+
   printf("  ret\n");
   return 0;
 }
